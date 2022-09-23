e79635a09a0ffa57c1048735345fc5c4aa1fb2d4
William <wifryo@gmail.com>
2022-09-23 07:19:17 +0200

    Implement backend

--- src/App.js
+++ src/App.js
@@ -7,36 +7,76 @@ function App() {
   const [guests, setGuests] = useState([]);
   const [isAttending, setIsAttending] = useState(false);
 
+  const baseUrl = 'http://localhost:4000';
   const handleEnter = (event) => {
     if (event.key === 'Enter') {
       addGuest();
     }
   };
-  function addGuest() {
+
+  /* function addGuest() {
     const newGuest = {
       firstName: firstName,
       lastName: lastName,
       isAttending: isAttending,
       id: Math.random().toPrecision(5),
     };
-    console.log(newGuest);
+    //console.log(newGuest);
     const newState = [newGuest, ...guests];
     setGuests(newState);
     setFirstName('');
     setLastName('');
     setIsAttending(false);
-  }
-  // duplicate IDs possible - amend!
-  let filteredArray = [];
-  function deleteGuest(id) {
-    filteredArray = guests.filter((obj) => {
-      return obj.id !== id;
+  } */
+
+  async function addGuest() {
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        attending: isAttending,
+      }),
     });
-    setGuests(filteredArray);
+    logGuests();
+  }
+  async function logGuests() {
+    const response = await fetch(`${baseUrl}/guests`);
+    const tempGuests = await response.json();
+    setGuests(tempGuests);
+    console.log(tempGuests);
   }
 
-  function logGuests() {
-    console.log(filteredArray);
+  async function deleteGuest(id) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'DELETE',
+    });
+    setGuests(await response.json());
+  }
+
+  async function setGuestAttendance(id, event) {
+    console.log('guest id', id);
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: event.currentTarget.checked }),
+    });
+    const updatedGuest = await response.json();
+    console.log(updatedGuest);
+  }
+
+  async function deleteAll() {
+    const tempArray = guests.map((guest) =>
+      fetch(`${baseUrl}/guests/${guest.id}`, {
+        method: 'DELETE',
+      }),
+    );
+    await Promise.all(tempArray);
   }
 
   return (
@@ -65,28 +105,32 @@ function App() {
               onKeyPress={handleEnter}
             />
             <br />
-            <label id="attending">Is this guest attending?</label>
-            <input
-              checked={isAttending}
-              type="checkbox"
-              id="attending"
-              onChange={(event) => setIsAttending(event.currentTarget.checked)}
-            />
           </form>
           <button onClick={() => addGuest()}>Add guest</button>
           <button onClick={() => logGuests()}>Log guests</button>
+          <button onClick={() => deleteAll()}>Delete guests (mean!)</button>
         </div>
         <br />
         Wow, your guest's name is {firstName} {lastName}? bit weird
         <div>
-          {guests.map((guest) => {
+          {guests.map((guest, index) => {
+            //console.log(index, 'index');
             return (
               <div key={guest.id}>
                 <h3>{`Name: ${guest.firstName} ${guest.lastName}`}</h3>
                 <p>Guest attending: {guest.isAttending ? 'yes' : 'no'}</p>
                 <button
                   onClick={() => deleteGuest(guest.id)}
-                >{`Delete ${guest.firstName} ${guest.lastName}`}</button>
+                >{`Remove ${guest.firstName} ${guest.lastName}`}</button>
+                {/* <button onClick={() => setGuestAttendance(guest.id)}>
+                  guest attendance test
+                </button> */}
+                <br />
+                <input
+                  checked={guest.isAttending}
+                  type="checkbox"
+                  onChange={(event) => setGuestAttendance(guest.id, event)}
+                />
               </div>
             );
           })}
